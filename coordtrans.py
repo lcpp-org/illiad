164a6472129fba3e81d0504e2be9f4f4bd744e64
spgula2 <spgula2@illinois.edu>
2024-03-31 02:36:49 -0500

    -Added placeholder coordinate transform to shift polar origin

--- coordtrans.py
+++ coordtrans.py
@@ -4,7 +4,7 @@ import numba as nb
 
 ## TRANSFORM TO CARTESIAN COORDINATES
 ##
-#@nb.jit(nb.types.Array(nb.float64, 1, "C")(nb.types.Array(nb.float64, 1, "C"), nb.float64), nopython=True)
+@nb.jit(nb.types.Array(nb.float64, 1, "C")(nb.types.Array(nb.float64, 1, "A"), nb.float64), nopython=True)
 def RTP_to_XYZ(p_RTP, Rmajor):
     # Function to take in a point defined in r-theta-phi coordinates
     # And return a point in Cartesian coordinates
@@ -58,9 +58,22 @@ def rot_vecXYZ_byPHI(vec_XYZ, delta_phi):
     # convention: +phi is clockwise when viewed from above
     rotated_XYZ = np.zeros(3)
     xFormMatrix = np.array([[ np.cos(delta_phi), -np.sin(delta_phi), 0.0],
-                            [ np.sin(delta_phi), np.cos(delta_phi), 0.0],
-                            [               0.0,               0.0, 1.0]])
+                            [ np.sin(delta_phi),  np.cos(delta_phi), 0.0],
+                            [               0.0,                0.0, 1.0]])
     
     rotated_XYZ = np.dot(vec_XYZ, xFormMatrix)
 
     return rotated_XYZ
+
+
+# transform from theta,r about (Geometric Axis)
+# to (Magnetic Axis): theta=pi, r=0.0187m
+def axisShift(rho, theta, r_delt_):
+
+    #rprime = np.sqrt(r_delt_**2 + rho**2 + 2*rho*r_delt_*np.cos(theta))
+    #thetaprime = np.arctan2( (rho*np.sin(theta)), r_delt_+rprime*np.cos(theta))
+    #if thetaprime<0.: thetaprime += 2*np.pi
+    #xformed_Coord = np.array([thetaprime, rprime])
+    
+    xformed_Coord = np.array([theta, rho])
+    return xformed_Coord
