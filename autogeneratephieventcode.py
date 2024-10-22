55e030b03639745caccd6c1f80a71d3c3c1a6da5
arnavg0yal <arnavrgoyal@gmail.com>
2024-10-22 18:02:55 -0500

    Final changes, it works now!

--- autogeneratephieventcode.py
+++ autogeneratephieventcode.py
@@ -1,12 +1,14 @@
 
-initial_lines = ["import numpy as np\n", "import numba as nb\n", "from coordtrans import XYZ_to_RTP\n", "from mesh import *\n", \
+
+
+
+
+def create_phi_events(name, eventspacing):
+    initial_lines = ["import numpy as np\n", "import numba as nb\n", "from coordtrans import XYZ_to_RTP\n", "from mesh import *\n", f"import {name[:-3]} \n", \
                 "#@nb.jit(nb.float64(nb.float64, nb.types.Array(nb.float64, 1, \"C\"), Mesh.class_type.instance_type), nopython=True)\n", \
                 "def inVV(t, p_XYZ, Mesh):\n", "	p_RTP = XYZ_to_RTP(p_XYZ[:3], Mesh.R0)\n", "	return Mesh.a - p_RTP[0]\n", \
                 "inVV.direction = -1.0\n", "inVV.terminal = True\n\n\n\n\n"]
-
-
-
-def create_phi_events(name, name2, eventspacing):
+    
     poincare_gen_lines = [f"    poincare_events = [ {name[:-3]}.inVV, \n"]
 
     if 360%eventspacing != 0:
@@ -22,55 +24,24 @@ def create_phi_events(name, name2, eventspacing):
             if degree != 360:
                 direction = 1.0
                 poincare_gen_lines.append(f"                        {name[:-3]}.isphi{degree}, \n")
+                event_lines = ["#@nb.jit(nb.float64(nb.float64, nb.types.Array(nb.float64, 1, \"C\"), Mesh.class_type.instance_type), nopython=True)\n", 
+                          f"def isphi{degree}(t, p_XYZ, Mesh):\n", "	p_RTP = XYZ_to_RTP(p_XYZ[:3], Mesh.R0)\n", f"	return p_RTP[2] - {i+1}. * (np.pi/{numEvents//2})\n",
+                          f"isphi{degree}.direction = {direction}\n\n"]
             else: 
                 direction = -1.0
-                poincare_gen_lines.append(f"                        {name[:-3]}.isphi{degree}] \n \n \n")
+                poincare_gen_lines.append(f"                        {name[:-3]}.isphi{degree}] \n")
             
-            event_lines = ["#@nb.jit(nb.float64(nb.float64, nb.types.Array(nb.float64, 1, \"C\"), Mesh.class_type.instance_type), nopython=True)\n", 
-                          f"def isphi{degree}(t, p_XYZ, Mesh):\n", "	p_RTP = XYZ_to_RTP(p_XYZ[:3], Mesh.R0)\n", "	return p_RTP[2] - 1. * (np.pi/20.)\n",
-                          f"isphi{degree}.direction = {direction}\n\n"]
+                event_lines = ["#@nb.jit(nb.float64(nb.float64, nb.types.Array(nb.float64, 1, \"C\"), Mesh.class_type.instance_type), nopython=True)\n", 
+                            f"def isphi{degree}(t, p_XYZ, Mesh):\n", f"	return p_XYZ[1]\n",
+                            f"isphi{degree}.direction = {direction}\n\n"]
+                
             with open(name, "a") as f:
                 f.writelines(event_lines)
-    
-    with open(name2, "r+") as f:
-        i = 0
-        
-        contents = f.readlines()
-        
-        start = 0
-        stop = 0
-        for line in contents:
-            if "## EVENT LIST" in line:
-                i+=1
-                start = i
-            elif "## SOLVER" in line and start != 0:
-                stop = i
-                break
-            else:
-                i+=1
-        
-        new_contents = contents[:start]
-        
-        for line in contents[stop:]:
-            new_contents.append(line)
-        
-        for j in range(len(poincare_gen_lines)):
-            new_contents.insert(start+j, poincare_gen_lines[j])
-        
-        new_contents[6] = f"import {name[:-3]} \n"
-        done = 0
-        for i,line in enumerate(new_contents):
-            if "phi_range = np.linspace( np.pi/20., 2*np.pi," in line:
-                new_contents[i] = f"    phi_range = np.linspace( np.pi/{180/eventspacing:.0f}., 2*np.pi, {360/eventspacing:.0f}) \n"
-                done = 1
-                
-            elif "with cf.ProcessPoolExecutor(max_workers=" in line and done == 1:
-                new_contents[i] = f"    with cf.ProcessPoolExecutor(max_workers={360/eventspacing:.0f}) as executor: \n"
-                
 
+        with open(name, "a") as f:
+            f.write("def eventsAndRange(): \n")
+            f.writelines(poincare_gen_lines)
+            f.write(f"    phi_range = np.linspace(np.pi/{180/eventspacing:.0f}., 2*np.pi, {360/eventspacing:.0f}) \n")
+            f.write("    return poincare_events,phi_range \n")
 
-        f.seek(0)
-        f.truncate()
-        f.writelines(new_contents)
-    
-create_phi_events("phi_events_5.py","variable_poincare_gen.py", 5)
+create_phi_events("phi_events.py", 1)
