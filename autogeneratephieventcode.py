b26a026c797e5f05db191a6c0ae74f84e74ef02c
arnavg0yal <arnavrgoyal@gmail.com>
2024-10-24 08:33:17 -0500

    changed string formatting from f to .format()

--- autogeneratephieventcode.py
+++ autogeneratephieventcode.py
@@ -4,12 +4,12 @@
 
 
 def create_phi_events(name, eventspacing):
-    initial_lines = ["import numpy as np\n", "import numba as nb\n", "from coordtrans import XYZ_to_RTP\n", "from mesh import *\n", f"import {name[:-3]} \n", \
+    initial_lines = ["import numpy as np\n", "import numba as nb\n", "from coordtrans import XYZ_to_RTP\n", "from mesh import *\n", "import {} \n".format(name[:-3]), \
                 "#@nb.jit(nb.float64(nb.float64, nb.types.Array(nb.float64, 1, \"C\"), Mesh.class_type.instance_type), nopython=True)\n", \
                 "def inVV(t, p_XYZ, Mesh):\n", "	p_RTP = XYZ_to_RTP(p_XYZ[:3], Mesh.R0)\n", "	return Mesh.a - p_RTP[0]\n", \
                 "inVV.direction = -1.0\n", "inVV.terminal = True\n\n\n\n\n"]
     
-    poincare_gen_lines = [f"    poincare_events = [ {name[:-3]}.inVV, \n"]
+    poincare_gen_lines = ["    poincare_events = [ {}.inVV, \n".format(name[:-3])]
 
     if 360%eventspacing != 0:
         return("error: bad spacing")
@@ -23,17 +23,17 @@ def create_phi_events(name, eventspacing):
             
             if degree != 360:
                 direction = 1.0
-                poincare_gen_lines.append(f"                        {name[:-3]}.isphi{degree}, \n")
+                poincare_gen_lines.append("                        {}.isphi{}, \n".format(name[:-3], degree))
                 event_lines = ["#@nb.jit(nb.float64(nb.float64, nb.types.Array(nb.float64, 1, \"C\"), Mesh.class_type.instance_type), nopython=True)\n", 
-                          f"def isphi{degree}(t, p_XYZ, Mesh):\n", "	p_RTP = XYZ_to_RTP(p_XYZ[:3], Mesh.R0)\n", f"	return p_RTP[2] - {i+1}. * (np.pi/{numEvents//2})\n",
-                          f"isphi{degree}.direction = {direction}\n\n"]
+                          "def isphi{}(t, p_XYZ, Mesh):\n".format(degree), "	p_RTP = XYZ_to_RTP(p_XYZ[:3], Mesh.R0)\n", "	return p_RTP[2] - {}. * (np.pi/{})\n".format(i+1, numEvents//2),
+                          "isphi{}.direction = {}\n\n".format(degree, direction)]
             else: 
                 direction = -1.0
-                poincare_gen_lines.append(f"                        {name[:-3]}.isphi{degree}] \n")
+                poincare_gen_lines.append("                        {}.isphi{}] \n".format(name[:-3], degree))
             
                 event_lines = ["#@nb.jit(nb.float64(nb.float64, nb.types.Array(nb.float64, 1, \"C\"), Mesh.class_type.instance_type), nopython=True)\n", 
-                            f"def isphi{degree}(t, p_XYZ, Mesh):\n", f"	return p_XYZ[1]\n",
-                            f"isphi{degree}.direction = {direction}\n\n"]
+                            "def isphi{}(t, p_XYZ, Mesh):\n".format(degree), "	return p_XYZ[1]\n",
+                            "isphi{}.direction = {}\n\n".format(degree, direction)]
                 
             with open(name, "a") as f:
                 f.writelines(event_lines)
@@ -41,7 +41,7 @@ def create_phi_events(name, eventspacing):
         with open(name, "a") as f:
             f.write("def eventsAndRange(): \n")
             f.writelines(poincare_gen_lines)
-            f.write(f"    phi_range = np.linspace(np.pi/{180/eventspacing:.0f}., 2*np.pi, {360/eventspacing:.0f}) \n")
+            f.write("    phi_range = np.linspace(np.pi/{}., 2*np.pi, {}) \n".format(int(180/eventspacing), int(360/eventspacing)))
             f.write("    return poincare_events,phi_range \n")
 
 create_phi_events("phi_events.py", 1)
