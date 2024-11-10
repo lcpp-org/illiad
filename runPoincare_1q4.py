5613e6e829af0bfe544e9898e0be6302ac89a1de
arnavg0yal <arnavrgoyal@gmail.com>
2024-11-10 01:52:12 -0600

    theta initial condition testing works - time for 1q4 31p 1000s was 6743 sec but 1q4 10p 10t 400s was 2987s - poincare plots were also better

--- runPoincare_1q4.py
+++ runPoincare_1q4.py
@@ -18,15 +18,17 @@ b_hidra = Mesh(R0=0.72, a=0.19)
 b_hidra.loadCartesianField(BX, BY, BZ, mesh_prd, errField=True)
 
 ## SET UP INITIAL POSITIONS IN RTP COORDS
-Nlines = 21 #21
-spins = 1000
+Nlines = 10 #21
+Angles = 5
+spins = 200
 length = (2*np.pi * b_hidra.R0) * spins
 IC_Rad = np.array(np.linspace( 0.120, 0.020, Nlines))
-ICs_RTP = np.array([[R, 0., 2*np.pi - (np.pi/5.)] for R in IC_Rad])
+IC_THETA = np.array(np.linspace(0, np.pi, Angles))
+ICs_RTP = np.array([[R, T, 2*np.pi - (np.pi/5.)] for R in IC_Rad for T in IC_THETA])
 
 ## CONVERT TO XYZ COORDS
-ICs_XYZ = np.zeros(shape=(Nlines, 3))
-for i in range(Nlines):
+ICs_XYZ = np.zeros(shape=(Nlines*Angles, 3))
+for i in range(Nlines*Angles):
     ICs_XYZ[i] = RTP_to_XYZ(ICs_RTP[i], b_hidra.R0)
 
 ## GENERATE POINCARE DATA
@@ -35,7 +37,7 @@ fieldlines = [fieldLine(init_cond, length) for init_cond in ICs_XYZ]
 tMax, Poincare_output, wallPt_output = Gen_Poincare(b_hidra, fieldlines, simOut, 'Poincare', 'LSODA', 1e-7, 1e-32)
 
 ## IDENTIFY LAST-CLOSED FLUX SURFACE
-LCFS_index = identifyLCFS(LCFStype='inner', iconds=IC_Rad, t_maxs=tMax, outputHandler=simOut)
+LCFS_index = identifyLCFS(LCFStype='inner', iconds=IC_Rad, t_maxs=tMax[::Angles], outputHandler=simOut)
 
 ## END RUN ##
 simOut.log.info('## SIM FINISHED ##\n\n\n\n')
