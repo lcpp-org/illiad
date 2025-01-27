mesh.py: - fixed bug casugin inaccurate field at r=0.0 - updated error field calculation based on initial field measurement results fro 1-25-2025 fieldPlot.py: - plots the field with some measurement results coordtrans.py: -update for calc speed in function RTP_to_XYZ

classes/mesh.py
@@ -45,7 +45,6 @@ class Mesh:
         self.periodicity: np.int32[:]
         self.errField: np.bool
 
-
     #def loadCartesianField(self, Bx_: np.ndarray, By_: np.ndarray, Bz_: np.ndarray, period_ = np.array([0, 1, 5], dtype=np.int32), errField=False):
     def loadCartesianField(self, file_path, period_ = np.array([0, 1, 5], dtype=np.int32), errField=False):
         """ 
@@ -114,7 +113,8 @@ class Mesh:
         #return np.abs(term1 + term2)
     
         dr, dtheta, dphi = np.abs( point2 - point1 )
-        return (self.R0 + point1[0] * np.cos(point1[1]) ) * point1[0] * dr * dtheta * dphi
+        #return (self.R0 + point1[0] * np.cos(point1[1]) ) * point1[0] * dr * dtheta * dphi
+        return (self.R0 + point1[0] * np.cos(point1[1]) ) * point2[0] * dr * dtheta * dphi
 
     def rot_vecXYZ_byPHI(self, vec_XYZ, delta_phi):
         """
@@ -158,6 +158,7 @@ class Mesh:
             # Cast the indices to the last element of the array
             # This is to make sure the interpolation function does not fail
             rindex_lo  = self.nr - 2
+            
         else:
             # Point is within the mesh, and we can find the indices
             # (here "lo" stands for lower bound
@@ -211,7 +212,7 @@ class Mesh:
 
             # calculate the wieght function as the volume of the point-antiNode subelement
             antiNode_subVolume = self.subElementVolume(point_RTP_local, antiNode_rtp)
-            
+            print(antiNode_subVolume)
             totalVolume += antiNode_subVolume
             local_vecXYZ += node_vecXYZ * antiNode_subVolume
 
@@ -227,10 +228,16 @@ class Mesh:
         global_vecXYZ = self.rot_vecXYZ_byPHI(local_vecXYZ, phi_rotation)
         
         if self.errField:
-            global_vecXYZ[0] += 0.0002 #0.0001414214 #0.0002
-            global_vecXYZ[1] -= 0.0002 #0.0001414214 #0.0002
+           # err_mag = np.sqrt(0.0002*0.0002 + 0.0002*0.0002)
+            err_mag = 2.828427E-4
+            #err_dir = 78. * np.pi/180
+            err_dir = 150. * np.pi/180
 
-            #global_vecXYZ[1] += 0.0002 #0.0002
+            global_vecXYZ[0] += err_mag * np.cos(err_dir)
+            global_vecXYZ[1] -= err_mag * np.sin(err_dir)
+
+            # global_vecXYZ[0] += 0.0002
+            # global_vecXYZ[1] -= 0.0002
 
         return global_vecXYZ, ph_localN
 

solver/fieldsolve.py
@@ -13,7 +13,7 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 #################
 
 ## NAME YOUR OUTPUT FILE
-output_name = 'It3500_Ih6390_Iv000_0p955_hires'
+output_name = 'It000_Ih7000_Iv000_0p955'
 
 ## DEFINE MESH RESOLUTION
 test = [20, 4, 10]
@@ -23,18 +23,26 @@ hi_res = [ 191, 180, 360 ] # dr=0.001m., dtheta=2deg., dphi=1deg.
 mesh_size = hi_res
 
 # REF. COIL CURRENTS NORMALLY RUN ON HIDRA
-##########################
-#      |[Amp]|[Amp]|[Amp]#
-# IOTA | I_T | I_H | I_V #
-# 1/3  | 486 | 900 |  00 #
-# 1/4  | 486 | 790 |  00 #
-# 1/5  | 486 | 710 |  00 #
-# 1/7  | 581 | 581 |  00 #
-##########################
+#############################
+#      | [Amp]| [Amp]| [Amp]#
+# IOTA |  I_T |  I_H |  I_V #
+# 1/3  |  486 |  900 |   00 #
+# 1/4  |  486 |  790 |   00 #
+# 1/5  |  486 |  710 |   00 #
+# 1/7  |  581 |  581 |   00 #
+# MAX. | 3500 | 7000 |   ?? #
+#############################
 # INPUT COIL CURRENTS:
-I_toro = 3500.
-I_heli = 6390.
+I_toro = 0.
+I_heli = 7000.
 I_vert = 0.
+
+# *TESTING* Multiplier applied to all currents
+# Used to model the attenuation of the fields due to the stainless-steel vacuum vessel
+att_mult = 1.0 #0.7
+I_toro *= att_mult
+I_heli *= att_mult
+I_vert *= att_mult
 
 # Multiplier applied to helical current,
 # Used in conjunction with Cartesian error field to reproduce HIDRA's actual B-field

utility/coordtrans.py
@@ -13,9 +13,9 @@ def RTP_to_XYZ(p_RTP, Rmajor):
     # convention: When looking at a cross-section to the right of the +z axis, +theta is counterclockwise
     # convention: +phi is clockwise when viewed from above
     r, theta, phi = p_RTP[:3]
-    
-    x = (Rmajor + r*np.cos(theta)) * np.cos(phi)
-    y = (-1) * (Rmajor + r*np.cos(theta)) * np.sin(phi)
+    temp_ = (Rmajor + r*np.cos(theta))
+    x = temp_ * np.cos(phi)
+    y = (-1) * temp_ * np.sin(phi)
     z = r * np.sin(theta)
     p_XYZ = np.array([x, y, z])
     