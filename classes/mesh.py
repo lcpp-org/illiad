0022b3af153a366f88707a0a85349a4ac68105f9
spgula2 <spgula2@illinois.edu>
2025-05-05 14:28:32 -0500

    Add loadScalarField and interpScalarField methods to Mesh class for scalar field handling

--- classes/mesh.py
+++ classes/mesh.py
@@ -35,6 +35,7 @@ class Mesh:
         self.Bx: np.float64[:][:][:]
         self.By: np.float64[:][:][:]
         self.Bz: np.float64[:][:][:]
+        self.value: np.float64[:][:][:]
         self.periodicity: np.int32[:]
         self.errField: np.bool
         self.err_mag = 2.828427E-4 * 1.2
@@ -49,6 +50,10 @@ class Mesh:
         This function loads a vector field as a 3-dimensional scalar array for each cartesian vector.
         The grid properties are assumed from the dimensions of the input arrays
         """
+        # self.Bx: np.float64[:][:][:]
+        # self.By: np.float64[:][:][:]
+        # self.Bz: np.float64[:][:][:]
+
         Bx_, By_, Bz_ = np.load(file_path)
 
         if Bx_.shape != By_.shape or Bx_.shape != Bz_.shape:
@@ -233,3 +238,146 @@ class Mesh:
             vecXYZ[1] -= self.err_mag * self.sin_err_dir
 
         return vecXYZ, ph_localN
+    
+
+    def loadScalarField(self, file_path, period_ = np.array([0, 1, 5], dtype=np.int32), errField=False, att_mult=1.0):
+        """ 
+        This function loads a vector field as a 3-dimensional scalar array for each cartesian vector.
+        The grid properties are assumed from the dimensions of the input arrays
+        """
+        # self.value: np.float64[:][:][:]
+
+        val_= np.load(file_path)
+        # transpose val_
+        val_ = np.transpose(val_, (2, 1, 0))
+
+
+        self.nr, self.ntheta, self.nphi = val_.shape
+        self.periodicity = period_
+        self.att_mult = att_mult
+        self.value = val_ * att_mult
+        self.errField = errField
+
+        print(f"self.value.shape: {self.value.shape}")
+        #print(f"self.value: {self.value}")
+        # r periodicity
+        if self.periodicity[0]:
+            self.r_max = self.a / self.periodicity[0]
+            self.dr = self.r_max / self.nr
+            self.r_min = self.dr
+        else:
+            self.r_max = self.a
+            self.dr = self.r_max / (self.nr-1)
+            self.r_min = 0.
+        
+        # theta periodicity
+        if self.periodicity[1]:
+            self.theta_max = (2*np.pi) / self.periodicity[1]
+            self.dtheta = self.theta_max / self.ntheta
+            self.theta_min = self.dtheta
+        else:
+            self.theta_max = (2*np.pi)
+            self.dtheta = self.theta_max / (self.ntheta-1)
+            self.theta_min = 0.
+        
+        # phi periodicity
+        if self.periodicity[2]:
+            self.phi_max = (2*np.pi) / self.periodicity[2]
+            self.dphi = self.phi_max / self.nphi
+            self.phi_min = self.dphi
+        else:
+            self.phi_max = (2*np.pi)
+            self.dphi = self.phi_max / (self.nphi-1)
+            self.phi_min = 0.
+
+    def interpScalarField(self, point_XYZ, Cart=True):
+        """
+        Method to return the interpolated field values at a point defined in Cartesian coordinates
+        Interpolation done via a weighted sum of field values at each node of the enclosing cell
+        The weight function is calculated as the volume of the octant opposed to the node
+        Get the location of the point in RTP coordinates,
+        keep in domains:
+        r:        0.0 -> r_max     (minor Radius)
+        theta: dtheta -> theta_max (2pi/Nperiods)
+        phi:     dphi -> phi_max   (2pi/Nperiods)
+        """
+        ## Sanitize input (or make sure we are passing torch tensors!)
+        Npts = 1 #Npts = point_XYZ.shape[0]
+
+        if Cart: point_RTP = XYZ_to_RTP(point_XYZ, self.R0) #.to(device)
+        else: point_RTP = point_XYZ #.to(device)
+        
+        # periodic boundarys
+        r_local  = point_RTP[0]
+        th_local = np.remainder(point_RTP[1], self.theta_max) # keep theta within 0 and theta_max!
+        ph_localN, ph_local = np.divmod(point_RTP[2], self.phi_max) # keep phi within 0 and phi_max!
+
+        scalarVal = np.zeros([1,Npts], dtype=np.float64) 
+        rindex : np.int16
+        thindex : np.int16
+        phindex : np.int16
+
+        # indices and local (within cell) values
+        thindex, th_el = np.divmod(th_local, self.dtheta)
+        phindex, ph_el = np.divmod(ph_local, self.dphi)
+        rindex, r_el = np.divmod(r_local, self.dr)
+        if r_local >= self.r_max:
+            rindex = self.nr - 2
+
+        r_low = rindex * self.dr
+        th_low = (thindex+1) * self.dtheta
+
+        invr_el = self.dr - r_el
+        invth_el = self.dtheta - th_el
+        invph_el = self.dphi - ph_el
+
+        r_lowr_el = (r_low + r_el/2) * r_el
+        r_localinvr_el = (r_local + invr_el/2) * invr_el
+        
+        cos_th_low = np.cos(th_low)
+        cos_th_local = np.cos(th_local)
+
+        cos_th_low_r_low = r_low * cos_th_low
+        cos_th_low_r_local = r_local * cos_th_low
+        cos_th_local_r_low = r_low * cos_th_local
+        cos_th_local_r_local = r_local * cos_th_local
+
+        # sub-element volumes
+        A1 = (self.R0 + cos_th_low_r_low)     * r_lowr_el      * th_el    #* ph_el
+        A2 = (self.R0 + cos_th_low_r_local)   * r_localinvr_el * th_el    #* ph_el
+        A3 = (self.R0 + cos_th_local_r_low)   * r_lowr_el      * invth_el #* ph_el
+        A4 = (self.R0 + cos_th_local_r_local) * r_localinvr_el * invth_el #* ph_el
+        A5 = (self.R0 + cos_th_low_r_low)     * r_lowr_el      * th_el    #* invph_el
+        A6 = (self.R0 + cos_th_low_r_local)   * r_localinvr_el * th_el    #* invph_el
+        A7 = (self.R0 + cos_th_local_r_low)   * r_lowr_el      * invth_el #* invph_el
+        A8 = (self.R0 + cos_th_local_r_local) * r_localinvr_el * invth_el #* invph_el
+        
+        Areas = np.array([A1, A2, A3, A4, A5, A6, A7, A8])
+        Areas[:4] *= ph_el
+        Areas[4:] *= invph_el
+
+        # indices of the 8 corner nodes of the cell
+        ir_hi = rindex + 1
+        ir_lo = rindex
+        ith_hi = thindex
+        ith_lo = thindex - 1
+        iph_hi = phindex
+        iph_lo = phindex - 1
+
+        # indices of the 8 corner nodes of the cell
+        index_array = np.array([[ir_hi, ith_hi, iph_hi], [ir_lo, ith_hi, iph_hi],
+                    [ir_hi, ith_lo, iph_hi], [ir_lo, ith_lo, iph_hi],
+                    [ir_hi, ith_hi, iph_lo], [ir_lo, ith_hi, iph_lo],
+                    [ir_hi, ith_lo, iph_lo], [ir_lo, ith_lo, iph_lo]], dtype=np.int16)
+        
+        # B field vectors at the 8 corner nodes
+        nodeVals = np.zeros((8, 1))
+        nodeVals[:, 0] = self.value[index_array[:, 0], index_array[:, 1], index_array[:, 2]]
+        # print(f"nodeVals: {nodeVals}")
+
+        # result is sum of B field vectors weighted with sub-element volumes
+        scalarVal = np.dot(Areas, nodeVals) / np.sum(Areas)
+        # print(f"scalarVal: {scalarVal}")
+
+
+        return scalarVal, ph_localN
