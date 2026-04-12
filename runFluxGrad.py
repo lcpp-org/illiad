51b1741adf0d778bf12b679659900d1a715c5e74
spgula2 <spgula2@illinois.edu>
2026-04-11 22:44:02 -0500

    Refactor flux calculation and gradient functions; update parameters and add new analysis script

---
+++ runFluxGrad.py
@@ -0,0 +1,48 @@
+import numpy as np
+import utility.Flux_Calculator as fc
+import utility.Flux_Interpolator as fi
+import utility.Flux_Gradientor as fg
+
+N_phi = 360
+input_params = {
+    'ANLYS_DIR': "AcceptedIota3_1500spins_atole-9",
+    'ANLYS_SUBDIR': "LCFS19_360x180_ARTICLE_smooth3e-5",
+    #'ANLYS_SUBDIR': "LCFS14_360x180_ARTICLE",
+    'FIELD_FILE_TOR': 'input_files/It1000_Ih000_Iv000_1p000_1p000_64bit.npy',
+    'FIELD_FILE_HEL': 'input_files/It000_Ih1000_Iv000_1p000_1p000_64bit.npy',
+    'CURRENT_TOR': 0.486, #[kA]
+    'CURRENT_HEL': 0.900, #[kA]
+    'CONFIG_TOR': 'default_toroidal',
+    'CONFIG_HEL': 'default_helical',
+    'ENABLE_ERRFIELD': True,
+    'LCFS_INDEX': 19,
+    'NPHI': N_phi,
+    'NTHETA': 180,
+    'PHI_GENs': np.linspace(360//N_phi, 360, N_phi),
+    'MAX_SUBSETS': 3,
+    'SMALLEST_ISLAND_INDEX': 47, #53 #39
+    'SMOOTH_FCTR': 3e-5, #7.5e-6 #baseline 1e-6
+    'INTEGRATE_EPSABS': 5e-1,
+    'INTEGRATE_EPSREL': 5e-2,
+    'ISLAND_ALGORITHM': 'histogram', # 'kmeans', 'spectral'
+    'HIST_BINS': 90,
+    'PLOT_ALL': True,
+    'BIG_MESH': False
+    }
+
+## RUN ANALYSIS
+#smallest_island_index = fc.fluxCalculator(input_params)
+input_params['SMALLEST_ISLAND_INDEX'] = 47
+
+## RUN ANALYSIS
+input_params['ALPHA'] = 1.0
+input_params['DEBUG'] = True
+input_params['LCFS_INDEX'] = 19
+input_params['INV_SURF_INDICES'] = [18,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59]
+input_params['GOOD_SURF_INDEX'] = -11
+
+fi.fluxInterpolator(input_params)
+
+# ## RUN ANALYSIS
+input_params['OUTPUT_FILE_NAME'] = "Efield_AcceptedIota3_lcfs19_noIslandMULTIQ"
+fg.fluxGradientor(input_params)
