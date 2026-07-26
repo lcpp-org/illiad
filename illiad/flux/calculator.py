11c0dcf6e5fb681c509494e252411524521e44e6
spgula2 <spgula2@illinois.edu>
2026-07-26 14:04:47 -0500

    Fix filename formatting in load_poincare_data methods for consistent output

--- illiad/flux/calculator.py
+++ illiad/flux/calculator.py
@@ -177,7 +177,7 @@ class FluxCalculator:
 
     def load_poincare_data(self, phi_deg):
         """Loads Poincare data for a specific toroidal angle."""
-        filename = 'Poincare_{:03d}.npy'.format(int(phi_deg))
+        filename = 'Poincare_{:03.0f}.npy'.format(phi_deg)
         flux_surfaces = self.simIO.loadNumpyData(filename, subdir="Poincare")
         return flux_surfaces
 
